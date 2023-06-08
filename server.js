597d25454071e7a295dc9942173f71b1f1707baa
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-06-08 16:59:33 +0530

    import database file into server.js

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8500;
 
+// require database connection file
+const dbConnection = require("./config/dbConnection");
+
 app.listen(PORT, (err) => {
   if (err) {
     console.log("server is not running on port");
