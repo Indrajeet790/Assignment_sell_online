b0c0d73b63f6ea3a0c77a0f1aaa533ec09bbffb1
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-06-10 00:05:03 +0530

    storeRoutes

--- routes/storeRoutes.js
+++ routes/storeRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 // const auth = require("../middleware/auth");
-const storeCtrl = require("../controller/subCategoryCtrl");
+const storeCtrl = require("../controller/storeController");
 
 //store routes
-router.post("/create",storeCtrl.addStoreInfo);
+router.post("/create", storeCtrl.addStoreInfo);
 
 module.exports = router;
