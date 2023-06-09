3b850d651d05dcc9a00462be81f64d6565fa7e5e
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-06-09 22:24:19 +0530

    update categoryRoutes

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const auth = require("../middleware/auth");
+// const auth = require("../middleware/auth");
 const createCategoryCtrl = require("../controller/categoryCtrl");
 
 //category routes
-router.post("/create", auth, createCategoryCtrl.createCategory);
+router.post("/create", createCategoryCtrl.createCategory);
 
 module.exports = router;
