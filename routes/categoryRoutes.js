085c9c4fcab24ae25ca1faa127420ae3b03b035e
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-06-09 14:33:39 +0530

    routes for category

---
+++ routes/categoryRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const auth = require("../middleware/auth");
+const createCategoryCtrl = require("../controller/categoryCtrl");
+
+//category routes
+router.post("/create", auth, createCategoryCtrl.createCategory);
+
+module.exports = router;
