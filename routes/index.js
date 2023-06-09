7f63cba9fc27b0736bd081ea6e028afc62417723
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-06-09 14:37:10 +0530

    require all routes into index.js file

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+
+// all routes are requires into a index.js file
+
+// seller routes
+router.use("/seller", require("./sellerRoutes"));
+//storeRoutes
+router.use("/store", require("./storeRoutes"));
+//subcategory routes
+router.use("/sub_category", require("./subCategoryRoutes"));
+// category routes
+router.use("/category", require("./categoryRoutes"));
+
+module.exports = router;
