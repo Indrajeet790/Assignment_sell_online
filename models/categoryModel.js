7859266464864551c44caa403c2b409d3973c324
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-06-09 14:33:09 +0530

    model for category

---
+++ models/categoryModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+// schema for category
+const categorySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+// TELLING MONGOOSE THAT IT IS A MODEL
+const Category = mongoose.model("Category", categorySchema);
+
+module.exports = Category;
