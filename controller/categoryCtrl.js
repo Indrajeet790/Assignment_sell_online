b95a6f27bdac056bc291d8a039321eb5736d3286
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-06-10 15:37:19 +0530

    category controller code

--- controller/categoryCtrl.js
+++ controller/categoryCtrl.js
@@ -1,34 +1,17 @@
 const Category = require("../models/categoryModel");
-const jwt = require("jsonwebtoken");
-let secretKey = "Secret";
 
-// create a category
 const createCategory = async (req, res) => {
   try {
-    //
-    let token;
-    const bearerHeader = req.headers["authorization"];
-    if (typeof bearerHeader !== "undefined") {
-      const bearer = bearerHeader.split(" ");
-      token = bearer[1];
+    const data = await Category.create(req.body);
+    if (data) {
+      return res.status(201).json(data);
     }
-    jwt.verify(token, secretKey, async (err, authData) => {
-      if (err) {
-        res.send({ message: "Invalid Seller" });
-      } else {
-        // find category name
-        const category = await Category.findOne({ name: req.body.name });
-        if (!category) {
-          // create category
-          const data = await Category.create(req.body);
-          res.status(201).json(data);
-        } else {
-          res.send({ message: "Category By Same Name Already Exist" });
-        }
-      }
+
+    return res.status(400).json({
+      error: "Store not created",
     });
   } catch (error) {
-    res.send({ error: error.message });
+    throw error;
   }
 };
 
