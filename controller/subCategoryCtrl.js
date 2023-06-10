65ac938675f12be7320758e547b9c94769e43317
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-06-10 15:38:13 +0530

    subcategory code

--- controller/subCategoryCtrl.js
+++ controller/subCategoryCtrl.js
@@ -1,35 +1,19 @@
 const SubCategory = require("../models/subCategoryModel");
-const jwt = require("jsonwebtoken");
-let secretKey = "Secret";
 
 const createSubCategory = async (req, res) => {
   try {
-    let token;
-    const bearerHeader = req.headers["authorization"];
-    if (typeof bearerHeader !== "undefined") {
-      const bearer = bearerHeader.split(" ");
-      token = bearer[1];
+    const subcategory = await SubCategory.findOne({ name: req.body.name });
+    if (!subcategory) {
+      // create sub_category
+      const data = await SubCategory.create(req.body);
+      res.status(201).json(data);
+    } else {
+      res.send({ message: "subCategory By Same Name Already Exist" });
     }
-    jwt.verify(token, secretKey, async (err, authData) => {
-      if (err) {
-        res.send({ message: "Invalid seller" });
-      } else {
-        // find sub_category
-        const subcategory = await SubCategory.findOne({ name: req.body.name });
-        if (!subcategory) {
-          // create sub_category
-          const data = await SubCategory.create(req.body);
-          res.status(201).json(data);
-        } else {
-          res.send({ message: "subCategory By Same Name Already Exist" });
-        }
-      }
-    });
   } catch (error) {
-    res.send({ error: error.message });
+    throw error;
   }
 };
-
 module.exports = {
   createSubCategory,
 };
